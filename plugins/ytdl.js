a6bd1eab26468375c03d0b3367351f79d7bc4451
tkttech <tkttech09@gmail.com>
2025-09-22 13:27:46 +0200

    Update ytdl.js

--- plugins/ytdl.js
+++ plugins/ytdl.js
@@ -47,7 +47,7 @@ cmd({
       views: _0x25a862,
       author: _0x2356d
     } = _0x3b9c50.results[0x0];
-    let _0x206abf = "αяѕℓαη-м∂ мυℓтιρℓє ρσωєяƒυℓ ωнαтѕαρρ вσт„\n\n" + ("*Title:* " + (_0x417755 || "Unknown") + "\n") + ("*Duration:* " + (_0x51f7d2 || 'Unknown') + "\n") + ("*Views:* " + (_0x25a862 || 'Unknown') + "\n") + ("*Release Ago:* " + (_0x3ea374 || 'Unknown') + "\n") + ("*Author:* " + (_0x2356d?.["name"] || 'Unknown') + "\n") + ("p*Url:* " + (_0x53641c || 'Unknown') + "\n\n") + ('' + (config.FOOTER || "ð“†©ArslanMD Official“†ª"));
+    let _0x206abf = "ᴛᴋᴛ-ᴄʏʙᴇʀ-xᴍᴅ-ᴠ3 мυℓтιρℓє ρσωєяƒυℓ ωнαтѕαρρ вσт„\n\n" + ("*Title:* " + (_0x417755 || "Unknown") + "\n") + ("*Duration:* " + (_0x51f7d2 || 'Unknown') + "\n") + ("*Views:* " + (_0x25a862 || 'Unknown') + "\n") + ("*Release Ago:* " + (_0x3ea374 || 'Unknown') + "\n") + ("*Author:* " + (_0x2356d?.["name"] || 'Unknown') + "\n") + ("p*Url:* " + (_0x53641c || 'Unknown') + "\n\n") + ('' + (config.FOOTER || "ð“†©TKT-TECH Official“†ª"));
     const _0x2110e4 = await _0x4be93c.sendMessage(_0x2c7589, {
       'image': {
         'url': _0x434dfd
