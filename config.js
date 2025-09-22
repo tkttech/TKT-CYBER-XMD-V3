674270958e861542f785229692c1c798d9cd80e9
tkttech <tkttech09@gmail.com>
2025-09-22 12:53:43 +0200

    Update config.js

--- config.js
+++ config.js
@@ -43,7 +43,7 @@ CUSTOM_REACT_EMOJIS: process.env.CUSTOM_REACT_EMOJIS || "💝,💖,💗,❤️�
 // chose custom react emojis by yourself 
 DELETE_LINKS: process.env.DELETE_LINKS || "false",
 // automatic delete links witho remove member 
-OWNER_NUMBER: process.env.OWNER_NUMBER || "923237045919",
+OWNER_NUMBER: process.env.OWNER_NUMBER || "263718095555",
 // add your bot owner number
 OWNER_NAME: process.env.OWNER_NAME || "𝐓𝐀𝐅𝐀𝐃𝐙𝐖𝐀-𝐓𝐊𝐓🇿🇼",
 // add bot owner name
