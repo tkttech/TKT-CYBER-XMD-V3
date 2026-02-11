e89849cf91349cb732056e82155c88c694170b25
TKTTECH1 <tafadzwakureya799@gmail.com>
2026-02-11 19:28:15 +0200

    Update settings.js

--- settings.js
+++ settings.js
@@ -1,16 +1,16 @@
 const settings = {
-  packname: 'TUNZY-MD',
-  author: 'TUNZY',
-  botName: "TUNZY-MD",
-  botOwner: 'TUNZY', // Your name
-  ownerNumber: '2250779042402', //Set your number here without + symbol, just add country code & number without any space
+  packname: 'TKT-CYBER-XMD',
+  author: 'TKT-TECH',
+  botName: "TKT-CYBER-XMD",
+  botOwner: 'TKT-TECH', // Your name
+  ownerNumber: '263781400473', //Set your number here without + symbol, just add country code & number without any space
   giphyApiKey: 'NrSjG6var2uiuSYDm0xTqCX0xcFgGj4s',
   commandMode: "public",
   maxStoreMessages: 20, 
   storeWriteInterval: 10000,
   description: "This is a bot for managing group commands and automating tasks.",
   version: "1.0.0",
-  updateZipUrl: "https://github.com/tunzy-shop/TUNZY-MD/archive/refs/heads/main.zip",
+  updateZipUrl: "https://github.com/tkttech/TKT-CYBER-XMD-V3/archive/refs/heads/main.zip",
 };
 
 module.exports = settings;
