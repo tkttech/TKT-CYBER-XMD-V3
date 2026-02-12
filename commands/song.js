fcd56be12cdcd2ca15389505350ea18bdd99b6f8
TKTTECH1 <tafadzwakureya799@gmail.com>
2026-02-12 13:32:58 +0200

    Update Yupra download API URL

--- commands/song.js
+++ commands/song.js
@@ -28,7 +28,7 @@ async function tryRequest(getter, attempts = 3) {
 }
 
 async function getYupraDownloadByUrl(youtubeUrl) {
-	const apiUrl = `https://api.yupra.my.id/api/downloader/ytmp3?url=${encodeURIComponent(youtubeUrl)}`;
+	const apiUrl = `https://arslan-apis.vercel.app/download/ytmp3?url=${encodeURIComponent(youtubeUrl)}`;
 	const res = await tryRequest(() => axios.get(apiUrl, AXIOS_DEFAULTS));
 	if (res?.data?.success && res?.data?.data?.download_url) {
 		return {
